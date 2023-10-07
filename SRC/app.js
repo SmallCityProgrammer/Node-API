f3ffcf86e583644d042a28e013031109556bdcbd
Eliezer Castro <smallcity.programmer@gmail.com>
2023-10-06 22:11:31 -0300

    novas features, separação dos routes do app

--- SRC/app.js
+++ SRC/app.js
@@ -1,38 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+
 const app = express();
 const router = express.Router();
+
+const indexRoute = require('./routes/index-route')
+const productRoute = require('./routes/product-route')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-
-const route = router.get('/', (req, res, next) => {
-  res.status(200).send({
-    title:"Node Store API",
-    version:"0.0.2"
-  });
-});
-
-const create = router.post('/', (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-  const id = req.params.id;
-  res.status(201).send({
-    id:id, 
-    item:req.body
-  });
-});
-
-const del = router.delete('/', (req, res, next) => {
-  res.status(200).send(req.body);
-});
-
-app.use('/', route);//C
-app.use('/products', create); //R
-app.use('/products', put); //U
-app.use('/products', del); //D
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
