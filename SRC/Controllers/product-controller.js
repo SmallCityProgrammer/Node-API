923c60a501ddceeeeb4d40b9ba7503b2533443a1
Eliezer Castro <smallcity.programmer@gmail.com>
2023-10-11 20:40:02 -0300

    nova aula

--- SRC/Controllers/product-controller.js
+++ SRC/Controllers/product-controller.js
@@ -67,6 +67,25 @@ exports.post = (req,res,next) => {
       });
 };
 
+exports.put = (req,res,next) => {
+  Product
+    .findByIdAndUpdate(req.params.id, {
+      $set: {
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price
+      }
+    }).then(x => {
+      res.status(201).send({
+        message: 'Produto atualizado com sucesso!'
+      });
+    }).catch(e => {
+      res.status(400).send({
+        message:'Falha ao atualizar produto',
+        data: e})
+    });
+};
+
 exports.put = (req,res,next) =>{
   const id = req.params.id;
   res.status(201).send({
