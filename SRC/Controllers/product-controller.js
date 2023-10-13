759a3a1708fbf52e3b9409d8c95c2351f1df5864
Eliezer Castro <smallcity.programmer@gmail.com>
2023-10-13 13:55:59 -0300

    novas aulas

--- SRC/Controllers/product-controller.js
+++ SRC/Controllers/product-controller.js
@@ -73,10 +73,11 @@ exports.put = (req,res,next) => {
       $set: {
         title: req.body.title,
         description: req.body.description,
-        price: req.body.price
+        price: req.body.price,
+        slug: req.body.slug
       }
     }).then(x => {
-      res.status(201).send({
+      res.status(200).send({
         message: 'Produto atualizado com sucesso!'
       });
     }).catch(e => {
@@ -86,15 +87,16 @@ exports.put = (req,res,next) => {
     });
 };
 
-exports.put = (req,res,next) =>{
-  const id = req.params.id;
-  res.status(201).send({
-    id:id, 
-    item:req.body
+
+exports.delete = (req,res,next) =>{Product
+  .findOneAndRemove(req.body.id)
+  .then(x => {
+    res.status(200).send({
+      message: 'Produto removido com sucesso!'
+    });
+  }).catch(e => {
+    res.status(400).send({
+      message:'Falha ao remover produto',
+      data: e})
   });
-};
-
-
-exports.delete = (req,res,next) =>{
-  res.status(201).send(req.body);
 };
--- SRC/routes/product-route.js
+++ SRC/routes/product-route.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const controller = require('../Controllers/product-controller')
 
 router.get('/', controller.get);
-router.get('/:slug', controller.getBySlug);
 router.get('/admin/:id', controller.getById);
+router.get('/:slug', controller.getBySlug);
 router.get('/tags/:tag', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
-router.delete('/', controller.delete);
+router.delete('/:id', controller.delete);
 
 module.exports = router;
