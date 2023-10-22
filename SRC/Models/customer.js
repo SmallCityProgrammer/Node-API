ddaa979b0c76d9f5c1a2cf06c69361fc843404a5
Eliezer Castro <smallcity.programmer@gmail.com>
2023-10-22 12:48:18 -0300

    adicionando roles ao customers

--- SRC/Models/customer.js
+++ SRC/Models/customer.js
@@ -15,7 +15,11 @@ const schema = new Schema({
   password:{
     type:String,
     required:true
-  }
+  },
+  roles:[{
+    type:String,
+    required:true
+  }]
 });
 
 module.exports = mongoose.model('Customer', schema);
